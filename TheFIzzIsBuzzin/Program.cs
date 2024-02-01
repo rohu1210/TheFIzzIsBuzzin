4852a3b71d792d9b2bb6eab4a1d7bb1ecbc6768e
rohu1210 <rohu1210@S26509>
2024-02-01 14:11:36 +0100

    Add project files.

---
+++ TheFIzzIsBuzzin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFIzzIsBuzzin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 15 == 0)// i är jämnt delbart på 15
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 2 != 0)
+                {
+                    Console.WriteLine($"Uneven {i}");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
